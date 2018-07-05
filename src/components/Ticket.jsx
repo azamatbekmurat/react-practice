7e3307eed44da8850c259d65fc6fb76200abf8a5
Guest User <Guest@epicodus-50.local>
2018-07-05 14:41:09 -0700

    add timer to the app

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -14,6 +14,7 @@ function Ticket(props){
       `}</style>
       <div className="color-toggle">
         <h3>{props.location} - {props.names}</h3>
+        <h4>{props.formattedWaitTime}</h4>
         <p><em>{props.issue}</em></p>
         <hr/>
       </div>
@@ -22,10 +23,11 @@ function Ticket(props){
   )
 }
 
+
 Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  issue: PropTypes.string
-
+  issue: PropTypes.string,
+  formattedWaitTime: PropTypes.string.isRequired
 }
 export default Ticket
